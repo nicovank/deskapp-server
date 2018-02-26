de42a8fb7210e39c63eeee3b723f26e0b612a4d7
nicovank <nvankemp@gmail.com>
2018-02-26 15:35:03 -0500

    Removed files specifying interface.

--- deskapp-server-api/src/main/java/com/oswego/reslife/deskapp/api/DefaultCommunicationService.java
+++ deskapp-server-api/src/main/java/com/oswego/reslife/deskapp/api/DefaultCommunicationService.java
@@ -1,19 +1,22 @@
-package com.oswego.reslife.deskapp.api;
+package com.oswego.reslife.deskapp.api.impl;
 
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Service;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
+@Service("communication")
 @Path("communication")
-public interface CommunicationService {
+public class DefaultCommunicationService {
 
 	@Path("list")
 	@POST
 	@Consumes({ MediaType.APPLICATION_JSON })
 	@Produces({ MediaType.APPLICATION_JSON })
-	String listMessages(@RequestBody String body);
+	public String listMessages(String body) {
+		return body;
+	}
 }
