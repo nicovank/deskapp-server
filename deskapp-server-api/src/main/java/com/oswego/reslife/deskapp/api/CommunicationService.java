d7b593ea0a2f41c71ee1cab1f3f948634622e962
nicovank <nvankemp@gmail.com>
2018-02-26 15:27:37 -0500

    Small modifications.

--- deskapp-server-api/src/main/java/com/oswego/reslife/deskapp/api/CommunicationService.java
+++ deskapp-server-api/src/main/java/com/oswego/reslife/deskapp/api/CommunicationService.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
 @Path("communication")
@@ -12,6 +13,7 @@ public interface CommunicationService {
 
 	@Path("list")
 	@POST
+	@Consumes({ MediaType.APPLICATION_JSON })
 	@Produces({ MediaType.APPLICATION_JSON })
 	String listMessages(@RequestBody String body);
 }
