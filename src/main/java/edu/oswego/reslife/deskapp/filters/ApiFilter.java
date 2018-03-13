3cd07b4b64c97f46bf9b86dbedd92636b8b88ecb
nicovank <nvankemp@gmail.com>
2018-03-13 16:09:26 -0400

    Added comment.

--- src/main/java/edu/oswego/reslife/deskapp/filters/ApiFilter.java
+++ src/main/java/edu/oswego/reslife/deskapp/filters/ApiFilter.java
@@ -11,6 +11,9 @@ public class ApiFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+
+		// Here we should check if the user is logged in and has access to whatever resource he is trying to load.
+
 		response.setContentType("application/json");
 		filterChain.doFilter(request, response);
 	}
