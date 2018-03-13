7d6efd11378f330381cfbf37582cafc93ca6d604
nicovank <nvankemp@gmail.com>
2018-03-13 02:15:48 -0400

    Added Employee model.

---
+++ src/main/java/edu/oswego/reslife/deskapp/api/sql/models/Employee.java
@@ -0,0 +1,106 @@
+package edu.oswego.reslife.deskapp.api.sql.models;
+
+public class Employee {
+
+	/**
+	 * Position of the employee. Must be coherent with the constraint on the table.
+	 */
+	public enum Position {
+		RHD, AHD, RA, DA
+	}
+
+	private String id;
+	private String building;
+	private String firstName;
+	private String lastName;
+	private Position position;
+	private String email;
+	private String hashedPassword;
+	private String phoneNb;
+
+	public void setID(String id) {
+		this.id = id;
+	}
+
+	public void setBuilding(String building) {
+		this.building = building;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * Utility, sets the position from the string.
+	 *
+	 * @param position a string representing the position of the employee.
+	 */
+	public void setPosition(String position) {
+		switch (position) {
+			case "RHD":
+				setPosition(Position.RHD);
+				break;
+			case "AHD":
+				setPosition(Position.AHD);
+				break;
+			case "RA":
+				setPosition(Position.RA);
+				break;
+			case "DA":
+				setPosition(Position.DA);
+				break;
+		}
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setHashedPassword(String hashedPassword) {
+		this.hashedPassword = hashedPassword;
+	}
+
+	public void setPhoneNb(String phoneNb) {
+		this.phoneNb = phoneNb;
+	}
+
+	public String getID() {
+		return id;
+	}
+
+	public String getBuilding() {
+		return building;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getHashedPassword() {
+		return hashedPassword;
+	}
+
+	public String getPhoneNb() {
+		return phoneNb;
+	}
+}
