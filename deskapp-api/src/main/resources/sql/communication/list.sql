c846a9fccc0a25754b86dfff5a7b0207625b6bf1
nicovank <nvankemp@gmail.com>
2018-03-23 19:36:57 -0400

    Working post message.

--- deskapp-api/src/main/resources/sql/communication/list.sql
+++ deskapp-api/src/main/resources/sql/communication/list.sql
@@ -1,5 +1,5 @@
-SELECT messages.ID, First_Name, Last_Name, Time, Message
-FROM messages, employees
+SELECT Messages.ID, First_Name, Last_Name, Time, Message
+FROM Messages JOIN Employees ON Employee_ID = Employees.ID
 WHERE Building_ID = ?
 ORDER BY ID DESC
 LIMIT ?, ?;
---
+++ deskapp-server/src/main/java/edu/oswego/reslife/deskapp/servlets/requests/AddMessageRequest.java
@@ -0,0 +1,9 @@
+package edu.oswego.reslife.deskapp.servlets.requests;
+
+public class AddMessageRequest {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+}
